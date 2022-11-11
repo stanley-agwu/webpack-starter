fc79a64338046c1460a00cb4c3e67339163f8537
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2022-11-11 10:32:05 +0100

    webpack setup - created webpack config file

--- webpack-starter/src/index.js
+++ webpack-starter/src/index.js
@@ -1,6 +1,3 @@
-
-import { v4 as uuidv4 } from 'uuid';
 import { generateJoke } from './generateJoke';
 
 console.log(generateJoke());
-console.log(uuidv4());
---
+++ webpack-starter/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+
+}
